db06d357aa1f4a993dfd5b06751f21aa4a6ca9ce
GColina <gcolinaramirez@gmail.com>
2024-07-20 17:19:35 +0200

    https://www.youtube.com/watch?v=UaR7GSNACsM&t=23167s
    Hora - 6:56
    
    PalmistryView Completed

--- app/src/main/java/com/gcolina/horoscoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/gcolina/horoscoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,11 +1,16 @@
 package com.gcolina.horoscoapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.gcolina.horoscoapp.R
@@ -26,6 +31,7 @@ class PalmistryFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
+            startCamera()
         } else {
             Toast.makeText(requireContext(), R.string.permission_denied, Toast.LENGTH_LONG).show()
         }
@@ -33,12 +39,37 @@ class PalmistryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initUi()
         if (checkCameraPermission()) {
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                try {
+                    cameraProvider.unbindAll()
+
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                } catch (e: Exception) {
+                    Log.e("aris", "Algo fue mal ${e.message}")
+                }
+            },
+            ContextCompat.getMainExecutor(requireContext())
+        )
     }
 
     private fun checkCameraPermission(): Boolean = PermissionChecker.checkSelfPermission(
